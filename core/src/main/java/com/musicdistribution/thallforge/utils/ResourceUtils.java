66bffcce3a131587a70b91851759920b2bbed0c6
Nikola Stanojkovski <65570806+nikolaStanojkovski@users.noreply.github.com>
2024-10-23 00:39:58 +0200

    Adjustment of the resource ID generation

--- core/src/main/java/com/musicdistribution/thallforge/utils/ResourceUtils.java
+++ core/src/main/java/com/musicdistribution/thallforge/utils/ResourceUtils.java
@@ -4,6 +4,7 @@ import com.drew.lang.annotations.Nullable;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,7 +13,8 @@ public final class ResourceUtils {
     public static String generateId(@Nullable Resource resource) {
         return Optional.ofNullable(resource)
                 .map(Resource::getPath)
-                .map(UUID::fromString)
+                .map(resourcePath -> resourcePath.getBytes(StandardCharsets.UTF_8))
+                .map(UUID::nameUUIDFromBytes)
                 .map(UUID::toString)
                 .orElse(StringUtils.EMPTY);
     }
