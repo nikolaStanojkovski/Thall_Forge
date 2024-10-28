aea101e57e870011c8755bfae21576213e911b11
Nikola Stanojkovski <65570806+nikolaStanojkovski@users.noreply.github.com>
2024-10-28 01:09:50 +0100

    Adjustment of the max-interaction-count logic

--- core/src/main/java/com/musicdistribution/thallforge/services/impl/TopFansRecognitionServiceImpl.java
+++ core/src/main/java/com/musicdistribution/thallforge/services/impl/TopFansRecognitionServiceImpl.java
@@ -5,6 +5,7 @@ import com.musicdistribution.thallforge.services.TopFansRecognitionService;
 import com.musicdistribution.thallforge.services.impl.models.TopFansRecognitionResourceModel;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.sling.api.resource.ModifiableValueMap;
+import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.osgi.service.component.annotations.Component;
 
@@ -26,6 +27,17 @@ public class TopFansRecognitionServiceImpl implements TopFansRecognitionService 
     public void setMaxInteractionCount(ResourceResolver resourceResolver, Long maxInteractionCount) {
         Optional.ofNullable(resourceResolver.getResource(ThallforgeConstants.Paths.VAR_THALLFORGE_TOP_FANS))
                 .map(topFansResource -> topFansResource.adaptTo(ModifiableValueMap.class))
-                .ifPresent(topFansValueMap -> topFansValueMap.put("topSiteInteractionsNumber", maxInteractionCount));
+                .ifPresent(topFansValueMap -> saveMaxInteractionCount(resourceResolver, topFansValueMap, maxInteractionCount));
+    }
+
+    private void saveMaxInteractionCount(ResourceResolver resourceResolver,
+                                         ModifiableValueMap topFansValueMap,
+                                         Long maxInteractionCount) {
+        try {
+            topFansValueMap.put("topSiteInteractionsNumber", maxInteractionCount);
+            resourceResolver.commit();
+        } catch (PersistenceException e) {
+            log.error("Could not save top-site interaction count {}", maxInteractionCount, e);
+        }
     }
 }
